1880c9f111a77ea4b2a158bc31e31c1d31af3799
Muhammet Halit Karakis <halidk@gmail.com>
2024-02-18 21:00:02 -0800

    handle the auth code using http server

--- get_credentials.py
+++ get_credentials.py
@@ -1,10 +1,15 @@
+import http.server
 import os
+import threading
+import webbrowser
 
 from google_auth_oauthlib.flow import Flow
 
 SCOPES = [
     # Add scopes you need here -- /auth/drive is an example
     "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive.metadata",
 ]
 
 # This is the output file
@@ -14,6 +19,38 @@ CREDENTIALS_FILE = "credentials.json"
 CLIENT_SECRET_FILE = "client_secret.json"
 
 
+class RedirectHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if "code" in self.path:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            self.wfile.write(
+                b"<html><head><title>Authentication Successful</title></head>"
+            )
+            self.wfile.write(b"<body><h2>Authentication successful.</h2>")
+            self.wfile.write(b"<p>You can now close this tab.</p></body></html>")
+
+            code = self.path.split("code=")[1].split("&")[0]
+            self.server.flow.fetch_token(code=code)
+            with open(CREDENTIALS_FILE, "w") as f:
+                f.write(self.server.flow.credentials.to_json())
+
+            print(f"Credentials saved to {CREDENTIALS_FILE}")
+            threading.Thread(target=self.server.shutdown, daemon=True).start()
+        else:
+            self.send_response(400)
+            self.end_headers()
+
+
+def run_server(flow):
+    server_address = ("", 8000)
+    httpd = http.server.HTTPServer(server_address, RedirectHandler)
+    httpd.flow = flow
+    httpd.serve_forever()
+
+
 def main():
     if not os.path.exists(CLIENT_SECRET_FILE):
         print(
@@ -21,22 +58,16 @@ def main():
         )
         return 1
 
-    # Auth code is not auto-handled yet. But the auth screen will try to redirect
-    # to localhost. You can manually copy the value of `code` url parameter.
     flow = Flow.from_client_secrets_file(
         CLIENT_SECRET_FILE, scopes=SCOPES, redirect_uri="http://127.0.0.1:8000"
     )
 
-    auth_url = flow.authorization_url(prompt="consent")
+    auth_url, _ = flow.authorization_url(prompt="consent")
 
-    print(f"Please go to this URL: {auth_url[0]}")
+    print(f"Opening url: {auth_url}")
+    webbrowser.open(auth_url)
 
-    code = input("Enter the authorization code: ")
-    flow.fetch_token(code=code)
-
-    with open(CREDENTIALS_FILE, "w") as f:
-        f.write(flow.credentials.to_json())
-        print(f"Credentials saved to {CREDENTIALS_FILE}")
+    run_server(flow)
 
     return 0
 
